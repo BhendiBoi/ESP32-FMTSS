d69683ae5b1e522a05c5aa960e70d9182ae990de
BhendiBoi <sunnyfromsouth@gmail.com>
2021-02-13 15:35:45 +0530

    removed my important network credentials XD wth am i doing

--- ESP32 FMTSS System/src/main.cpp
+++ ESP32 FMTSS System/src/main.cpp
@@ -14,8 +14,17 @@
 #include "esp_http_server.h"
 
 //Replace with your network credentials
-const char *ssid = "JioFiber -  2.4";
-const char *password = "Jio@1102";
+const char *ssid = "lol";
+const char *password = "lol";
+
+// Set your Static IP address
+IPAddress local_IP(192, 168, 1, 184);
+// Set your Gateway IP address
+IPAddress gateway(192, 168, 1, 1);
+
+IPAddress subnet(255, 255, 0, 0);
+IPAddress primaryDNS(8, 8, 8, 8); // optional
+IPAddress secondaryDNS(8, 8, 4, 4); // optional
 
 #define PART_BOUNDARY "123456789000000000000987654321"
 
